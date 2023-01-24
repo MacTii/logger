2b88b2d431069466e476c485aba0566ee9385365
MacTii <mactii41@gmail.com>
2023-01-24 17:26:33 +0100

    Change message in set_log and add severity level in log_message

--- logger.c
+++ logger.c
@@ -9,20 +9,20 @@ int main(int argc, char *argv[]) {
     }
 
     // application code here
-    log_message(STANDARD, "Application started");
+    log_message(logger_severity_lvl, "Application started");
 
     while(1)
     {
-        if (set_log(1, "Example message for log severity MIN") == 2) {
+        if (set_log(1, "Message for log severity MIN") == 2) {
             close_logger();
             return EXIT_SUCCESS;
         }
-        if (set_log(2, "Example message for log severity STANDARD") == 2)
+        if (set_log(2, "Message for log severity STANDARD") == 2)
         {
             close_logger();
             return EXIT_SUCCESS;
         }
-        if (set_log(3, "Example message for log severity MAX") == 2) {
+        if (set_log(3, "Message for log severity MAX") == 2) {
             close_logger();
             return EXIT_SUCCESS;
         }
